53581e82b722825b27c4278dc51b221b6ee91389
SeongChan Lee <foriequal@gmail.com>
2017-12-09 06:58:14 +0900

    Randomize tootip attribute

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import sweep_and_watch from "./sweep.js"
 import dummy_formular from "./formulars/dummy.js"
 
+const tooltipAttribute = "tooltip-" + Math.floor((Math.random() * 1000000) + 1);
+
 function append_rule(css) {
     var head = document.getElementsByTagName('head')[0]; 
     var newCss = document.createElement('style');
@@ -11,15 +13,15 @@ function append_rule(css) {
 }
 
 append_rule(`
-*:hover > [tooltip] {
+*:hover > [${tooltipAttribute}] {
     position: relative;
     visibility: hidden;
 }
-*:hover > [tooltip]:before {
+*:hover > [${tooltipAttribute}]:before {
     position: absolute;
     visibility: visible;
     font-size: 1rem;
-    content: attr(tooltip);
+    content: attr(${tooltipAttribute});
     bottom: calc(100% + 1em);
     background-color: black;
     box-shadow: 0px 0px 3px 3px black;
@@ -41,15 +43,15 @@ function attach_level(root, method) {
     if (level == null)
         return;
     
-    const tooltip = document.createElement("span");
-    tooltip.setAttribute("tooltip", "level: " + level);
+    const tooltipAnchor = document.createElement("span");
+    tooltipAnchor.setAttribute(tooltipAttribute, "level: " + level);
 
-    const already = root.querySelector("[tooltip]");
+    const already = root.querySelector(`[${tooltipAttribute}]`);
     if (already != null) {
         root.removeChild(already);
     }
     
-    root.insertAdjacentElement('afterbegin', tooltip);
+    root.insertAdjacentElement('afterbegin', tooltipAnchor);
     converted.add(root);
 }
 
